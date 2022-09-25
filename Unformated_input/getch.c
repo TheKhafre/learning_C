0f0150c2df37920f0fa9aac8b275c6179bdda3f3
TheKhafre <105213608+TheKhafre@users.noreply.github.com>
2022-09-25 20:58:43 +0100

    playing with unformatted  string

---
+++ Unformated_input/getch.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+char main ()
+{
+	char ch;
+	ch = getch ();
+	printf("%c", ch);
+}
---
+++ Unformated_input/getchar.c
@@ -0,0 +1,7 @@
+#include <stdio>
+char main()
+{
+	char ch;
+	ch = getchar();
+	printf("%c", ch);
+}
