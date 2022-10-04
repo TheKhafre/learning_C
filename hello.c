6a178ab519babfd0277b7481dd4c98a104f3ff82
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2022-10-04 18:21:29 +0100

    created source files

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints hello world
+ * return 0 on success
+ */
+
+int main ()
+{
+    printf("hello world\n");
+    getchar();
+    return 0;
+}
